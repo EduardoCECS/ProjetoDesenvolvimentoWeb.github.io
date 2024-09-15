0964dc68c54d67e9157fc718e382ff6bbd244d79
Carlos <152208835+EduardoCECS@users.noreply.github.com>
2024-09-15 11:48:21 -0300

    Update j.js

--- j.js
+++ j.js
@@ -1,8 +1,41 @@
 console.log('JavaScript carregado com sucesso.');
 
+function handleCredentialResponse(response) {
+  console.log("Encoded JWT ID token: " + response.credential);
+}
+
 window.onload = function() {
   console.log("Window loaded");
 
+if (!google || !google.accounts || !google.accounts.id) {
+  console.error("Google accounts ID not loaded.");
+  return;
+}
+
+google.accounts.id.initialize({
+  client_id: "1011092350451-uae6dichhsaf92ptqces58bnmfup756e.apps.googleusercontent.com",
+  callback: handleCredentialResponse
+});
+
+console.log("Google Sign-In initialized");
+
+const buttonDiv = document.getElementById('buttonDiv');
+if (buttonDiv) {
+  google.accounts.id.renderButton(
+    buttonDiv,
+    { theme: "outline", size: "large" }
+  );
+  console.log("Button rendered");
+} else {
+  console.error("Elemento com o ID 'buttonDiv' não encontrado.");
+}
+
+google.accounts.id.prompt((notification) => {
+  if (notification.getSkippedReason() === 'tap_outside') {
+      google.accounts.id.prompt();
+    }
+  }); 
+};
 function formatCPF(value) {
     value = value.replace(/\D/g, '');
     value = value.replace(/(\d{3})(\d)/, '$1.$2');
@@ -24,6 +57,6 @@ document.getElementById('iconesenha').addEventListener('click', function() {
     eyeIcon.src = 'img/olho.webp';
   } else {
     passwordField.type = 'password';
-    eyeIcon.src = 'img/olho.webp'; 
+    eyeIcon.src = 'img/olho.webp';
   }
 });
